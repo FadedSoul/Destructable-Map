06b1752edd8110d02ba5c782c42e6dae79e1d625
Niels <Niels97vet@hotmail.com>
2016-09-30 20:16:41 +0200

    Destructable-Map

---
+++ Destructable_Map/Assets/Scripts/MouseClick.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseClick : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject SpawnObject;
+    // Update is called once per frame
+    void Update()
+    {
+        //checks for input and spawns het object op mouse position
+        if (Input.GetButtonDown("Fire1"))
+        {
+            var mousePos = Input.mousePosition;
+            mousePos.z = 1.0f;       //zorgt ervoor dat het object niet achter andere spawned
+            var objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+            Instantiate(SpawnObject, objectPos, Quaternion.identity);
+        }
+    }
+}
---
+++ Destructable_Map/Assets/Scripts/Projectile.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+    [SerializeField]
+    private float travelLimit;
+
+    [SerializeField]
+    private CircleCollider2D blastRadius;
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        Debug.Log("help");
+
+        //checks if its the object we want to hit
+        if (coll.gameObject.tag == "Target")
+        {
+            coll.gameObject.GetComponent<GroundController>().DestroyGround(blastRadius);
+            Destroy(gameObject);
+        }
+    }
+
+    void Update() {
+
+        //makes sure the object is deleted when out of field
+        if (this.transform.position.y < travelLimit)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
